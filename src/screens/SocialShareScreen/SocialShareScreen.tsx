8e6b1e5da47fc5b9aeca2557102e39c5b80c7435
jmejiamu <jmejiamu@gmail.com>
2024-05-02 16:10:08 -0700

    feat/header

--- src/screens/SocialShareScreen/SocialShareScreen.tsx
+++ src/screens/SocialShareScreen/SocialShareScreen.tsx
@@ -1,12 +1,29 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { GoBackArrow } from "../../components/GoBackArrow";
+import { Entypo } from "@expo/vector-icons";
 
 export const SocialShareScreen = () => {
   return (
     <SafeAreaView>
-      <Text>SocialShareScreen</Text>
+      <View style={styles.headerConatiner}>
+        <GoBackArrow styleContainer={{ marginHorizontal: 0 }} />
+        <Text style={styles.headerTtitle}>Social Net</Text>
+        <Entypo name="dots-two-horizontal" size={24} color="black" />
+      </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerTtitle: {
+    fontSize: 22,
+    fontWeight: "700",
+  },
+  headerConatiner: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 15,
+  },
+});
